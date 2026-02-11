612eecd8ef9f269976f2514c1a3ae390e30c20c6
Joaquín Enrique Fuentes Rodríguez <joaquin.enriquefr@gmail.com>
2026-02-11 09:31:22 -0600

    Final Version

--- Lab1PrograDeMicros/Lab1PrograDeMicros/main.asm
+++ Lab1PrograDeMicros/Lab1PrograDeMicros/main.asm
@@ -1,11 +1,9 @@
 /*
-* Contador4Bits.asm
+* Sumador de 4 bits
 *
-* Creado: 6 de febrero del 2026
+* Creado: 11-feb-2026
 * Autor : Joaquín Fuentes
-* Descripción:
-* Contador binario de 4 bits con incremento y decremento
-* mediante dos pushbuttons con antirrebote por software
+* Descripción: Se tienen dos contadores de 4 bits que funcionan por separado, cuyos valores se pueden sumar. Se tiene una led que muestra carry.
 */
 /****************************************/
 // Encabezado (Definición de Registros, Variables y Constantes)
@@ -23,78 +21,155 @@ OUT     SPL, R16
 LDI     R16, HIGH(RAMEND)
 OUT     SPH, R16
 /****************************************/
+// Configuracion MCU
+
 SETUP:
-    ; -------- ENTRADAS --------
-    ; PD2–PD5 entradas
-    CBI DDRD, DDD2
-    CBI DDRD, DDD3
-    CBI DDRD, DDD4
-    CBI DDRD, DDD5
 
-    ; Pull-ups
-    LDI R16, (1<<PD2)|(1<<PD3)|(1<<PD4)|(1<<PD5)
-    OUT PORTD, R16
+	; -------- ENTRADAS (BOTONES EN PORTD) --------
+	CBI DDRD, DDD2
+	CBI DDRD, DDD3
+	CBI DDRD, DDD4
+	CBI DDRD, DDD5
+	CBI DDRD, DDD6
 
-    ; -------- SALIDAS --------
-    ; PORTB completo
-    LDI R16, 0xFF
-    OUT DDRB, R16
-    CLR R16
-    OUT PORTB, R16
+	LDI R16, (1<<PD2)|(1<<PD3)|(1<<PD4)|(1<<PD5)|(1<<PD6)
+	OUT PORTD, R16 ; PULLUPS
 
-    ; -------- INICIAL --------
-    CLR R20        ; contador 1
-    CLR R21        ; contador 2
-    IN  R16, PIND  ; estado previo
+	; -------- SALIDAS --------
+
+	; CONTADOR 1 (PB0-PB3)
+	LDI R16, 0X0F
+	OUT DDRB, R16
+
+	; CONTADOR 2 (PC0-PC3)
+	LDI R16, 0X0F
+	OUT DDRC, R16
+
+	; RESULTADO PB4-PB5
+	SBI DDRB, DDB4
+	SBI DDRB, DDB5
+
+	; RESULTADO PC4-PC5
+	SBI DDRC, DDC4
+	SBI DDRC, DDC5
+
+	; CARRY PD7
+	SBI DDRD, DDD7
+
+	; LIMPIAR SOLO SALIDAS (NO TOCAR PULLUPS)
+	CLR R16
+	OUT PORTB, R16
+	OUT PORTC, R16
+	CBI PORTD, PD7
+
+	; -------- INICIAL --------
+	CLR R20
+	CLR R21
+	IN  R16, PIND
+	ANDI R16, 0b01111100
 
 MAIN:
-    IN  R17, PIND
-    CP  R17, R16
-    BREQ MAIN
+	;ON/OFF?
+	IN  R17, PIND
+	ANDI R17, 0b01111100
+	CP   R17, R16
+	BREQ MAIN
 
-    RCALL DELAY
 
-    IN  R18, PIND
-    CP  R18, R17
-    BRNE MAIN
+	RCALL DELAY
 
-    MOV R16, R17
+	IN   R18, PIND
+	ANDI R18, 0b01111100
+	CP   R18, R17
+	BRNE MAIN
 
-    ; ---- CONTADOR 1 ----
-    ; PD2 suma
-    SBRS R17, PD2
-    INC R20
 
-    ; PD3 resta
-    SBRS R17, PD3
-    DEC R20
+	MOV R16, R17
 
-    ; ---- CONTADOR 2 ----
-    ; PD4 suma
-    SBRS R17, PD4
-    INC R21
+	; ---- CONTADOR 1 ----
+	SBRS R17, PD2
+	INC R20
 
-    ; PD5 resta
-    SBRS R17, PD5
-    DEC R21
+	SBRS R17, PD3
+	DEC R20
 
-    ; limitar a 4 bits
-    ANDI R20, 0x0F
-    ANDI R21, 0x0F
+	; ---- CONTADOR 2 ----
+	SBRS R17, PD4
+	INC R21
 
-    ; ---- SALIDA ----
-    MOV R22, R21
-    SWAP R22
-    ANDI R22, 0xF0
-    OR   R22, R20
-    OUT  PORTB, R22
+	SBRS R17, PD5
+	DEC R21
+	
+	;SOLO 4 BITS
+	ANDI R20, 0X0F
+	ANDI R21, 0X0F
 
-    RJMP MAIN
+	; ---- MOSTRAR CONTADORES ----
 
-; --------------------------
+	; PB0-PB3
+	IN R22, PORTB
+	ANDI R22, 0b11110000
+	OR R22, R20
+	OUT PORTB, R22
+
+	; PC0-PC3
+	IN R23, PORTC
+	ANDI R23, 0b11110000
+	OR R23, R21
+	OUT PORTC, R23
+
+	; ---- SUMA ----
+	SBRS R17, PD6
+	RCALL SUMA
+
+	RJMP MAIN
+
+
+SUMA:
+
+	;SUMA DE REGISTROS
+	MOV R24, R20
+	ADD R24, R21
+
+	;LÓGICA DEL CARRY
+	SBRC R24, 4 ;10000
+	SBI PORTD, PD7
+
+	SBRS R24, 4
+	CBI PORTD, PD7
+	;--------------
+	ANDI R24, 0X0F
+
+	; BIT0 -> PB4
+	SBRS R24, 0
+	CBI PORTB, PB4
+	SBRC R24, 0
+	SBI PORTB, PB4
+
+	; BIT1 -> PB5
+	SBRS R24, 1
+	CBI PORTB, PB5
+	SBRC R24, 1
+	SBI PORTB, PB5
+
+	; BIT2 -> PC4
+	SBRS R24, 2
+	CBI PORTC, PC4
+	SBRC R24, 2
+	SBI PORTC, PC4
+
+	; BIT3 -> PC5
+	SBRS R24, 3
+	CBI PORTC, PC5
+	SBRC R24, 3
+	SBI PORTC, PC5
+
+	RET
+
+
 DELAY:
-    LDI R19, 255
+	LDI R19, 200
 D1:
-    DEC R19
-    BRNE D1
-    RET
+	DEC R19
+	BRNE D1
+	RET
